68b30626b0c904487a2b3fb9511fe62748733065
david <david>
2020-09-12 11:36:48 +0100

    Run post-receive in git repo; cache gems locally

--- bin/post-receive.rb
+++ bin/post-receive.rb
@@ -6,5 +6,7 @@ pushed_refs = $stdin.readlines
 
 output, status = Open3.capture2e("git annex post-receive", stdin_data: pushed_refs.join("\n"))
 
+Dir.chdir('/srv/cheesy.at/git')
+system("bundle config set path /srv/cheesy.at/gems")
 system("bundle install")
 system("JEKYLL_ENV=production bundle exec jekyll build --strict --trace --destination /srv/cheesy.at/site --verbose --incremental")
