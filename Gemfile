d7bbedf5b5dd79fea155b8c8796acea3c51736b0
David Schmitt <david.schmitt@puppet.com>
2020-11-08 20:08:39 +0000

    Update importer script with deps and working values

--- Gemfile
+++ Gemfile
@@ -36,3 +36,11 @@ end
 group :tools do
   gem 'scrapi'
 end
+
+group :import do
+  gem 'jekyll-import'
+  gem 'unidecode'
+  gem 'sequel'
+  gem 'mysql2'
+  gem 'htmlentities'
+end
--- bin/import.rb
+++ bin/import.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/ruby
 
-require "jekyll-import";
-    JekyllImport::Importers::WordPress.run({
-      "dbname"         => "db",
-      "user"           => "user",
-      "password"       => "pw",
-      "host"           => "127.0.0.1",
-      "port"           => "3306",
-      "socket"         => "",
-      "table_prefix"   => "",
-      "site_prefix"    => "",
-      "clean_entities" => true,
-      "comments"       => true,
-      "categories"     => true,
-      "tags"           => true,
-      "more_excerpt"   => true,
-      "more_anchor"    => true,
-      "extension"      => "html",
-      "status"         => ["publish"]
-    })
+require "jekyll-import"
+
+JekyllImport::Importers::WordPress.run({
+  "dbname"         => "cheesy_wp",
+  "user"           => "cheesy",
+  "password"       => ENV["DB_PASS"],
+  "host"           => "127.0.0.1",
+  "port"           => "3306",
+  "socket"         => "",
+  "table_prefix"   => "",
+  "site_prefix"    => "",
+  "clean_entities" => true,
+  "comments"       => true,
+  "categories"     => true,
+  "tags"           => true,
+  "more_excerpt"   => true,
+  "more_anchor"    => true,
+  "extension"      => "html",
+  "status"         => ["publish"]
+})
