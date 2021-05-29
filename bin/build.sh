15bd89636b7ec58a16fff13085ced946bf7e101b
Christine Schmitt <christine@cheesy.at>
2021-05-29 17:15:10 +0100

    git-annex in cheesy@Christines-MacBook-Air.local:~/cheesy.at

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cd /srv/cheesy.at/git/
+
 git annex lock .
 git commit -m "lock server" || true # if no changes
 JEKYLL_ENV=production bundle exec jekyll build --strict --verbose --destination /srv/cheesy.at/site --incremental
