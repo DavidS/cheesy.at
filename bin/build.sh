e436c8af00ebc1ad87b8f7ac5f521fe3b884edd9
David Schmitt <david@black.co.at>
2021-06-22 10:00:31 +0100

    Ensure that gemset is up-to-date before trying to build

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,7 @@ set -e
 
 cd /srv/cheesy.at/git/
 
+bundle check || bundle install
 git annex lock .
 git commit -m "lock server" || true # if no changes
 JEKYLL_ENV=production bundle exec jekyll build --strict --verbose --destination /srv/cheesy.at/site
