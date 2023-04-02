8d3a25733796c186c4a4e30765f065a89d4f3c75
Christine Schmitt <christine@cheesy.at>
2023-04-02 08:17:46 +0200

    git-annex in cheesy@Christines-MacBook-Air.local:~/cheesy.at

--- bin/build.sh
+++ bin/build.sh
@@ -7,6 +7,6 @@ cd /srv/cheesy.at/git/
 bundle check || bundle install
 git annex lock .
 git commit -m "lock server" || true # if no changes
-JEKYLL_ENV=production bundle exec jekyll build --strict --verbose --destination /srv/cheesy.at/site 2>&1 | grep -vE '/srv/cheesy.at/gems|/usr/local/lib/ruby'
+JEKYLL_ENV=production bundle exec jekyll build --strict --verbose --destination /srv/cheesy.at/site
 
 echo "Build Successful at" $(date --iso=s)
